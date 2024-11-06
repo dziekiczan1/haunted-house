276e5f4ddb8faf095de69cabe37ba0987bd492da
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-11-06 18:33:25 +0100

    add bush texture

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { Timer } from 'three/addons/misc/Timer.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import {Timer} from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
 
 /**
@@ -66,6 +66,21 @@ roofColorTexture.wrapS = THREE.RepeatWrapping;
 roofARMTexture.wrapS = THREE.RepeatWrapping;
 roofNormalTexture.wrapS = THREE.RepeatWrapping;
 
+// Bush
+const bushColorTexture = textureLoader.load('/bush/leaves_forest_ground_1k/leaves_forest_ground_diff_1k.webp');
+const bushARMTexture = textureLoader.load('/bush/leaves_forest_ground_1k/leaves_forest_ground_arm_1k.webp');
+const bushNormalTexture = textureLoader.load('/bush/leaves_forest_ground_1k/leaves_forest_ground_nor_gl_1k.webp');
+
+bushColorTexture.colorSpace = THREE.SRGBColorSpace;
+
+bushColorTexture.repeat.set(2, 1);
+bushARMTexture.repeat.set(2, 1);
+bushNormalTexture.repeat.set(2, 1);
+
+bushColorTexture.wrapS = THREE.RepeatWrapping;
+bushARMTexture.wrapS = THREE.RepeatWrapping;
+bushNormalTexture.wrapS = THREE.RepeatWrapping;
+
 /**
  * House
  */
@@ -85,7 +100,7 @@ const floor = new THREE.Mesh(
         displacementBias: -0.1
     })
 );
-floor.rotation.x = - Math.PI * 0.5;
+floor.rotation.x = -Math.PI * 0.5;
 scene.add(floor);
 
 gui.add(floor.material, 'displacementScale').min(0).max(1).step(0.001).name('displacementScale');
@@ -135,24 +150,34 @@ house.add(door);
 
 // Bushes
 const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
-const bushMaterial = new THREE.MeshStandardMaterial();
+const bushMaterial = new THREE.MeshStandardMaterial({
+    map: bushColorTexture,
+    aoMap: bushARMTexture,
+    metalnessMap: bushARMTexture,
+    roughnessMap: bushARMTexture,
+    normalMap: bushNormalTexture
+});
 
 const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush1.scale.set(0.5, 0.5, 0.5);
 // bush1.setScalar(0.5); // Same as scale.set
 bush1.position.set(0.8, 0.2, 2.2);
+bush1.rotation.x = - 0.75;
 
 const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush2.scale.set(0.25, 0.25, 0.25);
 bush2.position.set(1.4, 0.1, 2.1);
+bush2.rotation.x = - 0.75;
 
 const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush3.scale.set(0.4, 0.4, 0.4);
 bush3.position.set(-0.8, 0.1, 2.2);
+bush3.rotation.x = - 0.75;
 
 const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
 bush4.scale.set(0.15, 0.15, 0.15);
 bush4.position.set(-1, 0.05, 2.6);
+bush4.rotation.x = - 0.75;
 
 house.add(bush1, bush2, bush3, bush4);
 
@@ -163,7 +188,7 @@ const graveMaterial = new THREE.MeshStandardMaterial();
 const graves = new THREE.Group();
 scene.add(graves);
 
-for(let i = 0; i < 30; i++) {
+for (let i = 0; i < 30; i++) {
 
     // Coordinates
     const angle = Math.random() * Math.PI * 2;
@@ -205,8 +230,7 @@ const sizes = {
     height: window.innerHeight
 };
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth;
     sizes.height = window.innerHeight;
@@ -248,8 +272,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const timer = new Timer();
 
-const tick = () =>
-{
+const tick = () => {
     // Timer
     timer.update();
     const elapsedTime = timer.getElapsed();
